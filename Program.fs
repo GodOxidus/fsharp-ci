36ba0960df885dc9226175b939e3987d4709c64b
Vorotynsky Maxim <vorotynsky.maxim@gmail.com>
2021-02-01 00:35:58 +0300

    initial

---
+++ Program.fs
@@ -0,0 +1,14 @@
+open System
+open FSharp.Data
+
+
+type MoexNews = XmlProvider<"https://iss.moex.com/iss/sitenews.xml">
+
+[<EntryPoint>]
+let main argv =
+    let data = MoexNews.Load "https://iss.moex.com/iss/sitenews.xml"
+
+    data.Datas.[0].Rows
+    |> Array.iter (fun x -> printfn "%s" x.Title.Value)
+
+    0 // return an integer exit code
